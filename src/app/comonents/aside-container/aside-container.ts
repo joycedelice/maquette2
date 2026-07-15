4a256d96e0fb042275f7b4305a4c786d0e2e2575
MAFO JOYCE <joycedelicemafo@gmail.com>
2026-07-15 17:40:51 +0200

    chore: ajout configuration vercel

--- src/app/comonents/aside-container/aside-container.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-aside-container',
-  imports: [],
-  templateUrl: './aside-container.html',
-  styleUrl: './aside-container.css',
-})
-export class AsideContainer {}
